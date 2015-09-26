f2dbabe14c15fd1d0eea66236f0a1504e14e2fa2
Ubuntu <ubuntu@ip-172-31-26-80.us-west-2.compute.internal>
2015-09-26 11:35:53 +0000

    working geolocation with showing on map. TODO: add websocket communication. add markers for pedestrian and bike icon

--- Settings.py
+++ Settings.py
@@ -1,7 +1,9 @@
 
 import os
-DEBUG = True
-DIRNAME = os.path.dirname(__file__)
-STATIC_PATH = os.path.join(DIRNAME, 'static')
+DEBUG         = True
+DIRNAME       = os.path.dirname(__file__)
+STATIC_PATH   = os.path.join(DIRNAME, 'static')
 TEMPLATE_PATH = os.path.join(DIRNAME, 'templates')
-DOMAIN = "ec2-52-11-102-108.us-west-2.compute.amazonaws.com"
+DOMAIN        = "ec2-52-11-102-108.us-west-2.compute.amazonaws.com"
+HERE_APP_ID   = "HU5QgdCj5nc5FNkCLwg4"
+HERE_APP_CODE = "6e4a3JK4hwtYrY3HPuPqdg"
